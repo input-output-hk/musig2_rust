56012f6b64a79a2e243f8c3ca3deb8b77b8f7544
curiecrypt <gamze.kilic@iohk.io>
2023-01-30 10:22:43 +0300

    converted to dalek ristretto

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
-extern crate secp256k1;
-use secp256k1::{PublicKey, Secp256k1, SecretKey, rand};
+#![allow(non_snake_case)]
+use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::constants;
 
+// use ed25519_dalek::{Keypair, Signature, Signer};
+// use rand_chacha_dalek_compat::ChaCha20Rng;
+// use rand_chacha_dalek_compat::rand_core::{OsRng};
+// use rand_core::{CryptoRng, RngCore};
+
+extern crate rand_core;
+
+
+
+pub struct KeyPair{
+    sk: Scalar,
+    pk: RistrettoPoint
+}
+
+pub struct Signer{
+    keypair: KeyPair,
+    comm_list: Option<Vec<KeyPair>>,
+    state: usize,
+}
+
+impl Signer{
+    fn musig2_key_gen() -> Self{
+        // let mut uniform_bytes = [0u8; 32];
+        // rng.fill_bytes(&mut uniform_bytes);
+        let sk = Scalar::from(999u64);
+        let pk = &sk * &constants::RISTRETTO_BASEPOINT_TABLE;
+        let keypair = KeyPair{sk, pk};
+        Self{
+            keypair,
+            comm_list: None,
+            state: 0,
+        }
+    }
+
+}
 
 
 fn main() {
-    let secp = Secp256k1::new();
-    let mut rng = rand::thread_rng();
-    // First option:
-    let (seckey, pubkey) = secp.generate_keypair(&mut rng);
-
-    assert_eq!(pubkey, PublicKey::from_secret_key(&secp, &seckey));
-
-    // Second option:
-    let seckey = SecretKey::new(&mut rng);
-    let _pubkey = PublicKey::from_secret_key(&secp, &seckey);
+    let signer = Signer::musig2_key_gen();
+    print!("{}", signer.keypair.sk.to_bytes().len());
 }
