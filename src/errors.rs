b44b8cfd567f84c6bbde6fbe66fb33c650912ac0
curiecrypt <gamze.kilic@iohk.io>
2023-02-17 18:33:21 +0300

    verify function added

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,12 @@
-use crate::{Commitment};
 
 /// Error types for aggregate signatures.
 #[derive(Debug, thiserror::Error, Eq, PartialEq)]
 pub enum AggrSignatureError {
     ///Aggregate nonce values differ
     #[error("Aggregate nonce values differ")]
-    NonceInvalid(Commitment),
+    NonceInvalid,
+
+    ///Aggregate signature is invalid
+    #[error("Aggregate signature is invalid")]
+    AggregateInvalid,
 }
